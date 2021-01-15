5f92de14a66600c99f7254278fe3ef82a8f8fdc5
App Academy Student <lxg073000@gmail.com>
2021-01-15 04:35:43 -0500

    Initialize Map Class and implement Map#set

--- w3d5/diy_adts.rb
+++ w3d5/diy_adts.rb
@@ -1,4 +1,5 @@
   class Stack
+    attr_reader :stack
     # Implement and test Stack class
 
     def initialize
@@ -9,21 +10,22 @@
 
     def push(el)
       # adds an element to the stack
-      @stack << el
+      stack << el
     end
 
     def pop
       # removes one element from the stack
-      @stack.shift 
+      stack.shift 
     end
 
     def peek
       # returns, but doesn't remove, the top element in the stack
-      @stack[0]
+      stack[0]
     end
   end
 
 class Queue
+  attr_reader :queue
   # Test the principle of FIFO for Queue class
 
   def initialize
@@ -31,15 +33,54 @@ class Queue
   end
 
   def enqueue(el)
-    @queue << el
+    queue << el
   end
 
   def dequeue
-    @queue.shift
+    queue.shift
   end
 
   def peek
-    @queue.first
+    queue.first
   end
+end
+
+class Map
+  
+  attr_reader :map
+  # Implement and test Map class
+  # Unique pair ADT implemented through a 2D Array.  each ele is a [k,v]
+
+  #Our Map class should have the following instance methods: set(key, value), get(key), delete(key), show
+  def initialize
+    @map = Array.new
+  end
+
+  def set(key, value)
+
+    #iterate through entire map.  Update value if key exists, otherwise create new k-v-pair
+    map.each do |pair|
+      if pair[0] == (key)
+        pair[1] = value
+        return true
+      end
+    end
+
+    map << [key,value]
+    true
+  end
+
+  def get(key)
+
+  end
+
+  def delete(key, value)
+    
+  end
+
+  def show
+
+  end
+  
 end
 
