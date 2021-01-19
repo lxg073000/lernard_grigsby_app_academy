8352f6e193195eda61c2cc769618a7b09e0b64e0
App Academy Student <lxg073000@gmail.com>
2021-01-19 05:41:23 -0500

    Raise errors for #valid_move? when out of range or cup is empty

--- W4D2/mancala/lib/board.rb
+++ W4D2/mancala/lib/board.rb
@@ -23,6 +23,8 @@ class Board
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos > 12 || start_pos < 0
+    raise "Starting cup is empty" if cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
