10ed839455ee99b1226bc98d8417024b6022e04a
App Academy Student <lxg073000@gmail.com>
2021-01-19 04:42:18 -0500

    Raise StandardError extensions for CoffeeErr, TummyAcheErr, and improper Bestfriend initializing

---
+++ W4D2/err_handling/user_script.rb
@@ -0,0 +1,9 @@
+require_relative 'super_useful'
+
+feed_me_a_fruit
+
+sam = BestFriend.new('Algelica Houston', 88, 'counting stacks')
+
+sam.talk_about_friendship
+sam.do_friendstuff
+sam.give_friendship_bracelet
