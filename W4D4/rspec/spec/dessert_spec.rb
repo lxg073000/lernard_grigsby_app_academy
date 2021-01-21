afe3394909c8dd320cc02c1b6291442596f93a08
App Academy Student <lxg073000@gmail.com>
2021-01-21 07:23:22 -0500

    Create Dessert#initialize tests

---
+++ W4D4/rspec/spec/dessert_spec.rb
@@ -0,0 +1,55 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  subject(:dessert) {Dessert.new("non-vegan", 2, 'Becky' )}
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect( dessert.type).to eq('non-vegan')
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(2)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{Dessert.new('vegan', 'two', 'becky')}.to raise_error(ArgumentError)
+    end
+
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient('bacon')
+      expect(dessert.ingredients).to include('bacon')
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array"
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
